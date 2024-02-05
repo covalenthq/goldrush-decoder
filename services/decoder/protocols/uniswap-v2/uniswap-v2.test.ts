2280f8e719d8578e5cb870597db67d1231cb26c5
Ganesh Swami <iamganesh.swami@gmail.com>
2024-02-04 19:22:06 -0800

    fix tests

--- services/decoder/protocols/uniswap-v2/uniswap-v2.test.ts
+++ services/decoder/protocols/uniswap-v2/uniswap-v2.test.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import app from "../../../..";
+import app from "../../../../api";
 import { type EventType } from "../../decoder.types";
 
 describe("source-zorb", () => {
