546c877394aa0a5c8ee97ef738ca84b22079f9ac
gurjotd23 <gurjot99@gmail.com>
2024-02-23 10:31:36 -0700

    added dfk feed pets

---
+++ services/decoder/protocols/dfk/dfk.decoders.ts
@@ -0,0 +1,67 @@
+import { GoldRushDecoder } from "../../decoder";
+import type { EventDetails } from "../../decoder.types";
+import { type EventType } from "../../decoder.types";
+import {
+    DECODED_ACTION,
+    DECODED_EVENT_CATEGORY,
+} from "../../decoder.constants";
+import { decodeEventLog, type Abi } from "viem";
+import PetABI from "./abis/dfk.pets.abi.json";
+
+GoldRushDecoder.on(
+    "dfk:PetFed",
+    ["defi-kingdoms-mainnet"],
+    PetABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: PetABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "PetFed",
+        }) as {
+            eventName: "PetFed";
+            args: {
+                fedBy: string;
+                petId: bigint;
+                foodType: bigint;
+                hungryAt: bigint;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Fed By",
+                value: decoded.fedBy,
+                type: "address",
+            },
+            {
+                heading: "Pet ID",
+                value: decoded.petId.toString(),
+                type: "text",
+            },
+            {
+                heading: "Food Type",
+                value: decoded.foodType.toString(),
+                type: "text",
+            },
+            {
+                heading: "Hungry At",
+                value: decoded.hungryAt.toString(),
+                type: "text",
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.GAMING,
+            name: "Pet Fed",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "DFK Pets",
+            },
+            details,
+        };
+    }
+);
---
+++ services/decoder/protocols/dfk/dfk.test.ts
@@ -0,0 +1,22 @@
+import request from "supertest";
+import app from "../../../../api";
+import { type EventType } from "../../decoder.types";
+
+describe("dfk", () => {
+    test("defi-kingdoms-mainnet:<EVENT NAME>", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "defi-kingdoms-mainnet",
+                tx_hash: "<ENTER TX HASH FOR TESTING>",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "<EVENT NAME>");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        const testAdded: boolean = false;
+        expect(testAdded).toEqual(true);
+    });
+});
