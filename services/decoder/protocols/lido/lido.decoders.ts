aeaa925d10fe1d798f509655d4fc22b3f69b0898
gurjotd23 <gurjot99@gmail.com>
2024-02-22 17:23:15 -0700

    added stETH events and tests

--- services/decoder/protocols/lido/lido.decoders.ts
+++ services/decoder/protocols/lido/lido.decoders.ts
@@ -7,7 +7,9 @@ import {
 } from "../../decoder.constants";
 import { decodeEventLog, type Abi } from "viem";
 import ABI from "./abis/lido.steth.abi.json";
-import { date } from "yup";
+import { addMethod, date } from "yup";
+import { prettifyCurrency } from "@covalenthq/client-sdk";
+import { TimestampParser } from "../../../../utils/functions";
 
 GoldRushDecoder.on(
     "lido:TransferShares",
@@ -42,7 +44,7 @@ GoldRushDecoder.on(
                 type: "address",
             },
             {
-                heading: "sharesValue",
+                heading: "Shares Value",
                 value: decoded.sharesValue.toString(),
                 type: "text",
             },
@@ -84,17 +86,17 @@ GoldRushDecoder.on(
 
         const details: EventDetails = [
             {
-                heading: "sender",
+                heading: "Sender",
                 value: decoded.sender,
                 type: "address",
             },
             {
-                heading: "to",
+                heading: "To",
                 value: decoded.amount.toString(),
                 type: "text",
             },
             {
-                heading: "referral",
+                heading: "Referral",
                 value: decoded.referral,
                 type: "address",
             },
@@ -103,11 +105,11 @@ GoldRushDecoder.on(
         const tokens: EventTokens = [
             {
                 heading: "Tokens Staked",
-                value: tx.value!.toString(),
-                decimals: tx.gas_metadata.contract_decimals,
-                ticker_symbol: tx.gas_metadata.contract_ticker_symbol,
-                ticker_logo: tx.gas_metadata.logo_url,
-                pretty_quote: tx.value_quote.toString(),
+                value: tx?.value!.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: tx?.value_quote?.toString(),
             },
         ];
 
@@ -145,48 +147,447 @@ GoldRushDecoder.on(
                 preTotalShares: bigint;
                 preTotalEther: bigint;
                 postTotalShares: bigint;
+                postTotalEther: bigint;
+                sharesMintedAsFees: bigint;
             };
         };
 
         const details: EventDetails = [
             {
-                heading: "Sender",
-                value: decoded.sender,
-                type: "address",
-            },
-            {
-                heading: "To",
-                value: decoded.amount.toString(),
+                heading: "Time Stamp",
+                value: decoded.reportTimestamp.toString(),
                 type: "text",
             },
             {
-                heading: "Referral",
-                value: decoded.referral,
-                type: "address",
+                heading: "Time Elapsed",
+                value: decoded.timeElapsed.toString(),
+                type: "text",
+            },
+            {
+                heading: "Pre-Total Shares",
+                value: decoded.preTotalShares.toString(),
+                type: "text",
+            },
+            {
+                heading: "Post-Total Shares",
+                value: decoded.postTotalShares.toString(),
+                type: "text",
+            },
+            {
+                heading: "Shares Minted As Fees",
+                value: decoded.postTotalShares.toString(),
+                type: "text",
             },
         ];
 
         const tokens: EventTokens = [
             {
-                heading: "Tokens Staked",
-                value: tx.value!.toString(),
-                decimals: tx.gas_metadata.contract_decimals,
-                ticker_symbol: tx.gas_metadata.contract_ticker_symbol,
-                ticker_logo: tx.gas_metadata.logo_url,
-                pretty_quote: tx.value_quote.toString(),
+                heading: "Pre-Total Ether",
+                value: decoded.preTotalEther.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.preTotalEther) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Post-Total Ether",
+                value: decoded.postTotalEther.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.postTotalEther) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
             },
         ];
 
         return {
             action: DECODED_ACTION.TRANSFERRED,
             category: DECODED_EVENT_CATEGORY.STAKING,
-            name: "Submitted",
+            name: "Token Rebased",
             protocol: {
                 logo: log_event.sender_logo_url as string,
                 name: "Lido" as string,
             },
             details,
             tokens,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "lido:SharesBurnt",
+    ["eth-mainnet"],
+    ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "SharesBurnt",
+        }) as {
+            eventName: "SharesBurnt";
+            args: {
+                account: string;
+                preRebaseTokenAmount: bigint;
+                postRebaseTokenAmount: bigint;
+                sharesAmount: bigint;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Account",
+                value: decoded.account,
+                type: "address",
+            },
+            {
+                heading: "Shares Amount",
+                value: decoded.sharesAmount.toString(),
+                type: "text",
+            },
+        ];
+
+        const date = TimestampParser(tx.block_signed_at, "YYYY-MM-DD");
+
+        const { data: StakingToken } =
+            await covalent_client.PricingService.getTokenPrices(
+                chain_name,
+                "USD",
+                log_event.sender_address,
+                {
+                    from: date,
+                    to: date,
+                }
+            );
+
+        const tokens: EventTokens = [
+            {
+                heading: "Pre-Rebase Token Amount",
+                value: decoded.preRebaseTokenAmount.toString(),
+                decimals: StakingToken?.[0]?.contract_decimals,
+                ticker_symbol: StakingToken?.[0]?.contract_ticker_symbol,
+                ticker_logo: StakingToken?.[0]?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (StakingToken?.[0]?.prices?.[0]?.price ?? 0) *
+                        (Number(decoded.preRebaseTokenAmount) /
+                            Math.pow(
+                                10,
+                                +(StakingToken?.[0]?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Post-Rebase Token Amount",
+                value: decoded.postRebaseTokenAmount.toString(),
+                decimals: StakingToken?.[0]?.contract_decimals,
+                ticker_symbol: StakingToken?.[0]?.contract_ticker_symbol,
+                ticker_logo: StakingToken?.[0]?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (StakingToken?.[0]?.prices?.[0]?.price ?? 0) *
+                        (Number(decoded.postRebaseTokenAmount) /
+                            Math.pow(
+                                10,
+                                +(StakingToken?.[0]?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.STAKING,
+            name: "Shares Burnt",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "Lido" as string,
+            },
+            details,
+            tokens,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "lido:ETHDistributed",
+    ["eth-mainnet"],
+    ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "ETHDistributed",
+        }) as {
+            eventName: "ETHDistributed";
+            args: {
+                reportTimestamp: bigint;
+                preCLBalance: bigint;
+                postCLBalance: bigint;
+                withdrawalsWithdrawn: bigint;
+                executionLayerRewardsWithdrawn: bigint;
+                postBufferedEther: bigint;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Time Stamp",
+                value: decoded.reportTimestamp.toString(),
+                type: "text",
+            },
+        ];
+
+        const tokens: EventTokens = [
+            {
+                heading: "Pre-CL Balance",
+                value: decoded.preCLBalance.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.preCLBalance) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Post-CL Balance",
+                value: decoded.postCLBalance.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.postCLBalance) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Withdrawals Withdrawn",
+                value: decoded.withdrawalsWithdrawn.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.withdrawalsWithdrawn) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Execution Layer Rewards Withdrawn",
+                value: decoded.executionLayerRewardsWithdrawn.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.executionLayerRewardsWithdrawn) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+            {
+                heading: "Post-Buffered Ether",
+                value: decoded.postBufferedEther.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.postBufferedEther) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.STAKING,
+            name: "ETH Distributed",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "Lido" as string,
+            },
+            details,
+            tokens,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "lido:WithdrawalsReceived",
+    ["eth-mainnet"],
+    ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "WithdrawalsReceived",
+        }) as {
+            eventName: "WithdrawalsReceived";
+            args: {
+                amount: bigint;
+            };
+        };
+
+        const tokens: EventTokens = [
+            {
+                heading: "Withdrawals Withdrawn",
+                value: decoded.amount.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.amount) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.STAKING,
+            name: "Withdrawals Received",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "Lido" as string,
+            },
+            tokens,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "lido:ELRewardsReceived",
+    ["eth-mainnet"],
+    ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "ELRewardsReceived",
+        }) as {
+            eventName: "ELRewardsReceived";
+            args: {
+                amount: bigint;
+            };
+        };
+
+        const tokens: EventTokens = [
+            {
+                heading: "Rewards Received",
+                value: decoded.amount.toString(),
+                decimals: tx?.gas_metadata?.contract_decimals,
+                ticker_symbol: tx?.gas_metadata?.contract_ticker_symbol,
+                ticker_logo: tx?.gas_metadata?.logo_url,
+                pretty_quote: prettifyCurrency(
+                    (tx?.gas_quote_rate ?? 0) *
+                        (Number(decoded.amount) /
+                            Math.pow(
+                                10,
+                                +(tx?.gas_metadata?.contract_decimals ?? 18)
+                            ))
+                ),
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.STAKING,
+            name: "EL Rewards Received",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "Lido" as string,
+            },
+            tokens,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "lido:CLValidatorsUpdated",
+    ["eth-mainnet"],
+    ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "CLValidatorsUpdated",
+        }) as {
+            eventName: "CLValidatorsUpdated";
+            args: {
+                preCLValidators: bigint;
+                postCLValidators: bigint;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Pre-CL Validators",
+                value: decoded.preCLValidators.toString(),
+                type: "text",
+            },
+            {
+                heading: "Post-CL Validators",
+                value: decoded.postCLValidators.toString(),
+                type: "text",
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.STAKING,
+            name: "CL Validators Updated",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: "Lido" as string,
+            },
+            details,
         };
     }
 );
--- services/decoder/protocols/lido/lido.test.ts
+++ services/decoder/protocols/lido/lido.test.ts
@@ -3,7 +3,7 @@ import app from "../../../../api";
 import { type EventType } from "../../decoder.types";
 
 describe("lido", () => {
-    test("eth-mainnet:TransferShares", async () => {
+    test("eth-mainnet:Transfer Shares", async () => {
         const res = await request(app)
             .post("/api/v1/tx/decode")
             .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
@@ -18,5 +18,132 @@ describe("lido", () => {
             throw Error("Event not found");
         }
         expect(event.details?.length).toEqual(3);
+    });
+
+    test("eth-mainnet:Submitted", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x7b820fc2409217a65ad6b7a4a06eeb412470b94821c07f7ddd4b75c8cbc172c4",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "Submitted");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.details?.length).toEqual(3);
+        expect(event.tokens?.length).toEqual(1);
+    });
+
+    test("eth-mainnet:Token Rebased", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "Token Rebased");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.details?.length).toEqual(5);
+        expect(event.tokens?.length).toEqual(2);
+    });
+
+    test("eth-mainnet:Shares Burnt", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "Shares Burnt");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.details?.length).toEqual(2);
+        expect(event.tokens?.length).toEqual(2);
+    });
+
+    test("eth-mainnet:ETH Distributed", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "ETH Distributed");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.details?.length).toEqual(1);
+        expect(event.tokens?.length).toEqual(5);
+    });
+
+    test("eth-mainnet:Withdrawals Received", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(
+            ({ name }) => name === "Withdrawals Received"
+        );
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.tokens?.length).toEqual(1);
+    });
+
+    test("eth-mainnet:EL Rewards Received", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "EL Rewards Received");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.tokens?.length).toEqual(1);
+    });
+
+    test("eth-mainnet:CL Validators Updated", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x406f1e0feb4259f667ef0ae2270933b2249fe8bd35f1eec36b8f15e987ee6e32",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(
+            ({ name }) => name === "CL Validators Updated"
+        );
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event.details?.length).toEqual(2);
     });
 });
