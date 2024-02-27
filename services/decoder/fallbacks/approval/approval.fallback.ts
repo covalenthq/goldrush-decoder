a7443e6aa59b2173b32004358f824ff07facde19
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-02-27 15:21:28 +0530

    !fix(approval): text + unlimited

--- services/decoder/fallbacks/approval/approval.fallback.ts
+++ services/decoder/fallbacks/approval/approval.fallback.ts
@@ -1,5 +1,9 @@
 import { GoldRushDecoder } from "../../decoder";
-import { type EventType } from "../../decoder.types";
+import {
+    type EventDetails,
+    type EventTokens,
+    type EventType,
+} from "../../decoder.types";
 import {
     DECODED_ACTION,
     DECODED_EVENT_CATEGORY,
@@ -28,9 +32,9 @@ GoldRushDecoder.fallback(
             abi: ABI,
             topics: raw_log_topics as [],
             data: raw_log_data as `0x${string}`,
-            eventName: "OrderFulfilled",
+            eventName: "Approval",
         }) as {
-            eventName: "OrderFulfilled";
+            eventName: "Approval";
             args: {
                 owner: string;
                 spender: string;
@@ -38,16 +42,60 @@ GoldRushDecoder.fallback(
             };
         };
 
-        const date = TimestampParser(block_signed_at, "YYYY-MM-DD");
-        const { data } = await covalent_client.PricingService.getTokenPrices(
-            chain_name,
-            "USD",
-            sender_address,
+        const unlimitedValue: boolean =
+            decoded.value.toString() ===
+            "115792089237316195423570985008687907853269984665640564039457584007913129639935";
+
+        const tokens: EventTokens = [];
+        const details: EventDetails = [
             {
-                from: date,
-                to: date,
-            }
-        );
+                heading: "Owner",
+                value: decoded.owner,
+                type: "address",
+            },
+            {
+                heading: "Spender",
+                value: decoded.spender,
+                type: "address",
+            },
+        ];
+
+        if (unlimitedValue) {
+            details.push({
+                heading: "Value",
+                value: "Unlimited",
+                type: "text",
+            });
+        } else {
+            const date = TimestampParser(block_signed_at, "YYYY-MM-DD");
+            const { data } =
+                await covalent_client.PricingService.getTokenPrices(
+                    chain_name,
+                    "USD",
+                    sender_address,
+                    {
+                        from: date,
+                        to: date,
+                    }
+                );
+
+            tokens.push({
+                heading: "Value",
+                value: decoded.value.toString(),
+                ticker_symbol: sender_contract_ticker_symbol,
+                ticker_logo: sender_logo_url,
+                decimals: sender_contract_decimals ?? 18,
+                pretty_quote: prettifyCurrency(
+                    data?.[0]?.items?.[0]?.price *
+                        (Number(decoded.value) /
+                            Math.pow(
+                                10,
+                                data?.[0]?.items?.[0]?.contract_metadata
+                                    ?.contract_decimals ?? 18
+                            ))
+                ),
+            });
+        }
 
         return {
             action: DECODED_ACTION.SWAPPED,
@@ -57,36 +105,8 @@ GoldRushDecoder.fallback(
                 logo: sender_logo_url as string,
                 name: sender_name as string,
             },
-            details: [
-                {
-                    heading: "Owner",
-                    value: decoded.owner,
-                    type: "address",
-                },
-                {
-                    heading: "Spender",
-                    value: decoded.spender,
-                    type: "address",
-                },
-            ],
-            tokens: [
-                {
-                    heading: "Value",
-                    value: decoded.value.toString(),
-                    ticker_symbol: sender_contract_ticker_symbol,
-                    ticker_logo: sender_logo_url,
-                    decimals: sender_contract_decimals ?? 18,
-                    pretty_quote: prettifyCurrency(
-                        data?.[0]?.items?.[0]?.price *
-                            (Number(decoded.value) /
-                                Math.pow(
-                                    10,
-                                    data?.[0]?.items?.[0]?.contract_metadata
-                                        ?.contract_decimals ?? 18
-                                ))
-                    ),
-                },
-            ],
+            details: details,
+            tokens: tokens,
         };
     }
 );
--- services/decoder/fallbacks/approval/approval.test.ts
+++ services/decoder/fallbacks/approval/approval.test.ts
@@ -3,7 +3,7 @@ import app from "../../../../api";
 import { type EventType } from "../../decoder.types";
 
 describe("fallback", () => {
-    test("Transfer", async () => {
+    test("Approval", async () => {
         const res = await request(app)
             .post("/api/v1/tx/decode")
             .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
@@ -13,11 +13,15 @@ describe("fallback", () => {
                     "0xa16a05cb1d0d37e75e52fa15e4d69307272ae922e098a5023017729ce95c358c",
             });
         const { events } = res.body as { events: EventType[] };
-        const event = events.find(({ name }) => name === "Transfer");
+        const event = events.find(({ name }) => name === "Approval");
         if (!event) {
             throw Error("Event not found");
         }
-        expect(event.details?.length).toEqual(2);
-        expect(event.tokens?.length).toEqual(1);
+        if (event.tokens?.length) {
+            expect(event.tokens?.length).toEqual(1);
+            expect(event.details?.length).toEqual(2);
+        } else {
+            expect(event.details?.length).toEqual(3);
+        }
     });
 });
