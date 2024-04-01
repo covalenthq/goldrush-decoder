9e94b0fb5084b270dc4b443ee1456b819525bfb6
Karan Pargal <karanpargal007@gmail.com>
2024-04-02 00:17:44 +0530

    feat: add FT and NFT decoding

--- services/decoder/protocols/defi-kingdoms/defi-kingdoms.decoders.ts
+++ services/decoder/protocols/defi-kingdoms/defi-kingdoms.decoders.ts
@@ -1,5 +1,5 @@
 import { GoldRushDecoder } from "../../decoder";
-import type { EventDetails } from "../../decoder.types";
+import type { EventDetails, EventNFTs, EventTokens } from "../../decoder.types";
 import { type EventType } from "../../decoder.types";
 import {
     DECODED_ACTION,
@@ -9,6 +9,7 @@ import { decodeEventLog, type Abi } from "viem";
 import PetABI from "./abis/defi-kingdoms.pets.abi.json";
 import HERO_AUCTION_ABI from "./abis/defi-kingdoms.hero-auction.abi.json";
 import { TimestampParser } from "../../../../utils/functions";
+import { prettifyCurrency } from "@covalenthq/client-sdk";
 
 GoldRushDecoder.on(
     "defi-kingdoms:PetFed",
@@ -96,6 +97,88 @@ GoldRushDecoder.on(
             };
         };
 
+        const date = TimestampParser(tx.block_signed_at, "YYYY-MM-DD");
+
+        // INFO: Fetching Jewel Token Price from Avalanche Mainnet as it is a native token on Defi Kingdoms
+
+        const { data: JewelToken } =
+            await covalent_client.PricingService.getTokenPrices(
+                "avalanche-mainnet",
+                "USD",
+                "0x997ddaa07d716995de90577c123db411584e5e46",
+                {
+                    from: date,
+                    to: date,
+                }
+            );
+
+        const { data: HeroNFT } =
+            await covalent_client.NftService.getNftMetadataForGivenTokenIdForContract(
+                chain_name,
+                "0xEb9B61B145D6489Be575D3603F4a704810e143dF", // Hero NFT Contract Address
+                decoded.tokenId.toString(),
+                {
+                    withUncached: true,
+                }
+            );
+
+        const nfts: EventNFTs = [
+            {
+                heading: `Auction Created`,
+                collection_address: HeroNFT?.items?.[0]?.contract_address,
+                collection_name:
+                    HeroNFT?.items?.[0]?.nft_data?.external_data?.name || null,
+                token_identifier: decoded.tokenId.toString(),
+                images: {
+                    default:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data?.image ||
+                        null,
+                    256:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_256 || null,
+                    512:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_512 || null,
+                    1024:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_1024 || null,
+                },
+            },
+        ];
+
+        const tokens: EventTokens = [
+            {
+                decimals: JewelToken?.[0]?.contract_decimals,
+                heading: "Starting Price",
+                value: String(decoded.startingPrice),
+                pretty_quote: prettifyCurrency(
+                    JewelToken?.[0]?.prices?.[0]?.price *
+                        (Number(decoded.startingPrice) /
+                            Math.pow(
+                                10,
+                                JewelToken?.[0]?.contract_decimals ?? 0
+                            ))
+                ),
+                ticker_logo: JewelToken?.[0]?.logo_urls?.token_logo_url,
+                ticker_symbol: JewelToken?.[0]?.contract_ticker_symbol,
+            },
+            {
+                decimals: JewelToken?.[0]?.contract_decimals,
+                heading: "Ending Price",
+                value: String(decoded.endingPrice),
+                pretty_quote: prettifyCurrency(
+                    JewelToken?.[0]?.prices?.[0]?.price *
+                        (Number(decoded.endingPrice) /
+                            Math.pow(
+                                10,
+                                JewelToken?.[0]?.contract_decimals ?? 0
+                            ))
+                ),
+                ticker_logo: JewelToken?.[0]?.logo_urls?.token_logo_url,
+                ticker_symbol: JewelToken?.[0]?.contract_ticker_symbol,
+            },
+        ];
+
         const details: EventDetails = [
             {
                 heading: "Auction ID",
@@ -143,6 +226,8 @@ GoldRushDecoder.on(
                 name: "DeFi Kingdoms",
             },
             details,
+            tokens,
+            nfts,
         };
     }
 );
@@ -167,6 +252,40 @@ GoldRushDecoder.on(
             };
         };
 
+        const { data: HeroNFT } =
+            await covalent_client.NftService.getNftMetadataForGivenTokenIdForContract(
+                chain_name,
+                "0xEb9B61B145D6489Be575D3603F4a704810e143dF", // Hero NFT Contract Address
+                decoded.tokenId.toString(),
+                {
+                    withUncached: true,
+                }
+            );
+
+        const nfts: EventNFTs = [
+            {
+                heading: `Auction Cancelled`,
+                collection_address: HeroNFT?.items?.[0]?.contract_address,
+                collection_name:
+                    HeroNFT?.items?.[0]?.nft_data?.external_data?.name || null,
+                token_identifier: decoded.tokenId.toString(),
+                images: {
+                    default:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data?.image ||
+                        null,
+                    256:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_256 || null,
+                    512:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_512 || null,
+                    1024:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_1024 || null,
+                },
+            },
+        ];
+
         const details: EventDetails = [
             {
                 heading: "Auction ID",
@@ -189,6 +308,7 @@ GoldRushDecoder.on(
                 name: "DeFi Kingdoms",
             },
             details,
+            nfts,
         };
     }
 );
@@ -214,6 +334,71 @@ GoldRushDecoder.on(
                 winner: string;
             };
         };
+
+        const date = TimestampParser(tx.block_signed_at, "YYYY-MM-DD");
+
+        const { data: JewelToken } =
+            await covalent_client.PricingService.getTokenPrices(
+                "avalanche-mainnet",
+                "USD",
+                "0x997ddaa07d716995de90577c123db411584e5e46",
+                {
+                    from: date,
+                    to: date,
+                }
+            );
+
+        const tokens: EventTokens = [
+            {
+                decimals: JewelToken?.[0]?.contract_decimals,
+                heading: "Total Price",
+                value: String(decoded.totalPrice),
+                pretty_quote: prettifyCurrency(
+                    JewelToken?.[0]?.prices?.[0]?.price *
+                        (Number(decoded.totalPrice) /
+                            Math.pow(
+                                10,
+                                JewelToken?.[0]?.contract_decimals ?? 0
+                            ))
+                ),
+                ticker_logo: JewelToken?.[0]?.logo_urls?.token_logo_url,
+                ticker_symbol: JewelToken?.[0]?.contract_ticker_symbol,
+            },
+        ];
+
+        const { data: HeroNFT } =
+            await covalent_client.NftService.getNftMetadataForGivenTokenIdForContract(
+                chain_name,
+                "0xEb9B61B145D6489Be575D3603F4a704810e143dF", // Hero NFT Contract Address
+                decoded.tokenId.toString(),
+                {
+                    withUncached: true,
+                }
+            );
+
+        const nfts: EventNFTs = [
+            {
+                heading: `Auction Successful`,
+                collection_address: HeroNFT?.items?.[0]?.contract_address,
+                collection_name:
+                    HeroNFT?.items?.[0]?.nft_data?.external_data?.name || null,
+                token_identifier: decoded.tokenId.toString(),
+                images: {
+                    default:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data?.image ||
+                        null,
+                    256:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_256 || null,
+                    512:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_512 || null,
+                    1024:
+                        HeroNFT?.items?.[0]?.nft_data?.external_data
+                            ?.image_1024 || null,
+                },
+            },
+        ];
 
         const details: EventDetails = [
             {
@@ -247,6 +432,8 @@ GoldRushDecoder.on(
                 name: "DeFi Kingdoms",
             },
             details,
+            tokens,
+            nfts,
         };
     }
 );
