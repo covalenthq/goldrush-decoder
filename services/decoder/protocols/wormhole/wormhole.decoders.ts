1aaa2970be1c8a29f01356d546501f1b1f9a1d45
Karan Pargal <83785975+karanpargal@users.noreply.github.com>
2024-04-19 23:08:57 +0530

    feat: Add Wormhole Decoders (#46)

---
+++ services/decoder/protocols/wormhole/wormhole.decoders.ts
@@ -0,0 +1,151 @@
+import { GoldRushDecoder } from "../../decoder";
+import type { EventDetails } from "../../decoder.types";
+import { type EventType } from "../../decoder.types";
+import {
+    DECODED_ACTION,
+    DECODED_EVENT_CATEGORY,
+} from "../../decoder.constants";
+import { decodeEventLog, type Abi } from "viem";
+import PORTAL_BRIDGE_ABI from "./abis/wormhole-portal-bridge.abi.json";
+import ETH_CORE_ABI from "./abis/wormhole-eth-core.abi.json";
+
+GoldRushDecoder.on(
+    "wormhole:TransferRedeemed",
+    [
+        "eth-mainnet",
+        "arbitrum-mainnet",
+        "bsc-mainnet",
+        "matic-mainnet",
+        "avalanche-mainnet",
+        "emerald-paratime-mainnet",
+        "aurora-mainnet",
+        "celo-mainnet",
+        "moonbeam-mainnet",
+        "optimism-mainnet",
+        "base-mainnet",
+    ],
+    PORTAL_BRIDGE_ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: PORTAL_BRIDGE_ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "TransferRedeemed",
+        }) as {
+            eventName: "TransferRedeemed";
+            args: {
+                emitterChainId: string;
+                emitterAddress: string;
+                sequence: bigint;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Emitter Chain ID",
+                value: decoded.emitterChainId,
+                type: "text",
+            },
+            {
+                heading: "Emitter Address",
+                value: decoded.emitterAddress,
+                type: "address",
+            },
+            {
+                heading: "Sequence",
+                value: decoded.sequence.toLocaleString(),
+                type: "text",
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.RECEIVED_BRIDGE,
+            category: DECODED_EVENT_CATEGORY.BRIDGE,
+            name: "TransferRedeemed",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: log_event.sender_name as string,
+            },
+            details,
+        };
+    }
+);
+
+GoldRushDecoder.on(
+    "wormhole:LogMessagePublished",
+    [
+        "eth-mainnet",
+        "arbitrum-mainnet",
+        "bsc-mainnet",
+        "matic-mainnet",
+        "avalanche-mainnet",
+        "emerald-paratime-mainnet",
+        "aurora-mainnet",
+        "celo-mainnet",
+        "moonbeam-mainnet",
+        "optimism-mainnet",
+        "base-mainnet",
+        "gnosis-mainnet",
+    ],
+    ETH_CORE_ABI as Abi,
+    async (log_event, tx, chain_name, covalent_client): Promise<EventType> => {
+        const { raw_log_data, raw_log_topics } = log_event;
+
+        const { args: decoded } = decodeEventLog({
+            abi: ETH_CORE_ABI,
+            topics: raw_log_topics as [],
+            data: raw_log_data as `0x${string}`,
+            eventName: "LogMessagePublished",
+        }) as {
+            eventName: "LogMessagePublished";
+            args: {
+                sender: string;
+                consistencyLevel: bigint;
+                sequence: bigint;
+                nonce: bigint;
+                payload: string;
+            };
+        };
+
+        const details: EventDetails = [
+            {
+                heading: "Sender",
+                value: decoded.sender,
+                type: "address",
+            },
+            {
+                heading: "Consistency Level",
+                value: decoded.consistencyLevel.toLocaleString(),
+                type: "text",
+            },
+            {
+                heading: "Sequence",
+                value: decoded.sequence.toLocaleString(),
+                type: "text",
+            },
+            {
+                heading: "Nonce",
+                value: decoded.nonce.toLocaleString(),
+                type: "text",
+            },
+            {
+                heading: "Payload",
+                value: decoded.payload,
+                type: "text",
+            },
+        ];
+
+        return {
+            action: DECODED_ACTION.TRANSFERRED,
+            category: DECODED_EVENT_CATEGORY.BRIDGE,
+            name: "LogMessagePublished",
+            protocol: {
+                logo: log_event.sender_logo_url as string,
+                name: log_event.sender_name as string,
+            },
+            details,
+        };
+    }
+);
---
+++ services/decoder/protocols/wormhole/wormhole.test.ts
@@ -0,0 +1,41 @@
+import request from "supertest";
+import app from "../../../../api";
+import { type EventType } from "../../decoder.types";
+
+describe("wormhole", () => {
+    test("eth-mainnet:LogMessagePublished", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x126f334fc80dc36189b2b1ef6c0fce2fcca4b16b287cf5ce8a7394a3c6710ba3",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "LogMessagePublished");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event?.details?.length).toEqual(5);
+    });
+});
+
+describe("wormhole", () => {
+    test("eth-mainnet:TransferRedeemed", async () => {
+        const res = await request(app)
+            .post("/api/v1/tx/decode")
+            .set({ "x-covalent-api-key": process.env.TEST_COVALENT_API_KEY })
+            .send({
+                chain_name: "eth-mainnet",
+                tx_hash:
+                    "0x3fbb9deb7b0e93bc0d474dbbea82371199430f560439851cdf5a64034344ef2c",
+            });
+        const { events } = res.body as { events: EventType[] };
+        const event = events.find(({ name }) => name === "TransferRedeemed");
+        if (!event) {
+            throw Error("Event not found");
+        }
+        expect(event?.details?.length).toEqual(3);
+    });
+});
