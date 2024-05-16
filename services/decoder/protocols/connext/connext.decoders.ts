44b5616f9a2fc157809c3ca616cdcfa3d50ef712
Karan Pargal <karanpargal007@gmail.com>
2024-05-16 18:58:26 +0530

    feat: Shift constants to decoder

--- services/decoder/protocols/connext/connext.decoders.ts
+++ services/decoder/protocols/connext/connext.decoders.ts
@@ -8,9 +8,20 @@ import {
 import { decodeEventLog, type Abi } from "viem";
 import ROUTER_ABI from "./abis/connext-router.abi.json";
 import CALLER_ABI from "./abis/connext-call.abi.json";
-import { DOMAIN_ID_TO_CHAIN_ID } from "./constants/domain-mapping";
 import { timestampParser } from "../../../../utils/functions";
 import { prettifyCurrency } from "@covalenthq/client-sdk";
+
+const DOMAIN_ID_TO_CHAIN_ID: { [domain_id: number]: string } = {
+    6648936: "Ethereum Mainnet",
+    1886350457: "Polygon",
+    1869640809: "Optimism",
+    1634886255: "Arbitrum One",
+    6778479: "Gnosis Chain",
+    6450786: "BNB Chain",
+    1818848877: "Linea",
+    1835365481: "Metis",
+    1650553709: "Base",
+};
 
 GoldRushDecoder.on(
     "connext:RouterLiquidityAdded",
