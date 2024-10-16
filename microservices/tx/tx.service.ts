1f26e1f83e1d1af336a89ab92adbc69c28d60aac
Yashvardhan Jagnani <yashjagnani73@gmail.com>
2024-10-17 04:22:40 +0530

    chore: optimize

--- microservices/tx/tx.service.ts
+++ microservices/tx/tx.service.ts
@@ -1,26 +1,24 @@
 import { GoldRushDecoder } from "../../services";
 import { type QueryOptions } from "../../services/decoder/decoder.types";
 import {
-    GoldRushClient,
     type Chain,
     type ChainName,
+    type GoldRushClient,
     type Transaction,
 } from "@covalenthq/client-sdk";
 
 export const fetchTxFromHash = async (
     chain_name: Chain,
     tx_hash: string,
-    goldrushApiKey: string
+    goldrush_client: GoldRushClient
 ): Promise<Transaction> => {
-    const goldrushClient = new GoldRushClient(goldrushApiKey);
     const { data, error_code, error_message } =
-        await goldrushClient.TransactionService.getTransaction(
+        await goldrush_client.TransactionService.getTransaction(
             chain_name,
             tx_hash,
             {
                 noLogs: false,
                 quoteCurrency: "USD",
-                withNftSales: false,
                 withSafe: false,
             }
         );
@@ -38,13 +36,13 @@ export const fetchTxFromHash = async (
 export const decodeLogsFromTx = async (
     chain_name: ChainName,
     tx: Transaction,
-    apiKey: string,
+    goldrush_client: GoldRushClient,
     options: QueryOptions
 ) => {
     const events = await GoldRushDecoder.decode(
         chain_name,
         tx,
-        apiKey,
+        goldrush_client,
         options
     );
     return events;
